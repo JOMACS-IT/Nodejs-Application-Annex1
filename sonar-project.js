fc092773834b8640c223c0cccf24f23ca1cfe593
ForkahEH <127892742+ForkahEH@users.noreply.github.com>
2023-05-11 22:57:00 +0000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.30.64:9000/',
+  serverUrl: 'http://172.31.25.75:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
